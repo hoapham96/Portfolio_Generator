3b0bf7a7fd64fbbf9c054754ba15147d37db0962
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-01-12 22:59:50 -0600

    print inquiter and show how to use

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+  console.log(inquirer)
 const fs = require('fs');
 
 const profileDataArgs = process.argv.slice(2);
@@ -11,3 +15,13 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
